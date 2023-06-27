21a9934f79788560a0f51228867407f49459aaf0
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-06-27 16:11:47 +0330

    [feat] add tasklist component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import ListItem from './components/listItem.vue';
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="bg-[#111111] min-h-screen">
+    <div class="max-w-5xl m-auto p-3">
+      <ListItem>Here goes task list Here goes task list Here goes task list</ListItem>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/listItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="h-48 w-48 bg-[#737373] p-5 text-white rounded-xl flex flex-col justify-between">
+        <p><slot></slot></p>
+        <div class="flex justify-between">
+            <button>
+                <img src="../assets/check.svg" alt="check" class="h-5 w-5">
+            </button>
+            <button @click="$emit('deleteItem')">
+                <img src="../assets/x.png" alt="remove" class="h-5 w-5">
+            </button>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+import { ref } from "vue";
+defineEmits(['deleteItem']);
+let isDone = ref(false);
+</script>
