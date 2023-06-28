0dbef37069cb0084e2757b14de3a198221b16847
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-06-28 14:47:14 +0330

    [feat] disable btn if input is empty

--- src/components/inputTask.vue
+++ src/components/inputTask.vue
@@ -2,7 +2,7 @@
     <div>
         <form action="#" @submit.prevent="submitHandler" class="py-7">
             <input type="text" class="h-8 w-72 mr-3 rounded-2xl p-1" v-model="TaskValue">
-            <button type="submit" class=" bg-slate-400  p-1 rounded-xl">Add task</button>
+            <button type="submit" class=" bg-slate-400  p-1 rounded-xl" :disabled="!TaskValue">Add task</button>
         </form>
     </div>
 </template>
